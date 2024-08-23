79516940eb52b97d9cdcbec23fedb26660fbc95a
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-23 20:02:58 +0200

    Get list of all users

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/users", (req, res) => {
+  const users = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Bob Williams" },
+    { id: 3, name: "Shannon Jackson" },
+  ];
+
+  res.send(`
+        <h1 class="text-2xl font-bold my-4">Users</h1>
+        <ul>
+            ${users.map((user) => `<li>${user.name}</li>`).join("")}
+        </ul>
+    `);
+});
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
