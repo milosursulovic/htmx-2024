0b5bdbcd3d1b2cc1fe3e19c91cc1f9b055e804ad
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-24 16:19:37 +0200

    Add query param

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.use(express.json());
 
 app.get("/users", (req, res) => {
   setTimeout(async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const limit = +req.query.limit || 10;
+
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+    );
     const users = await response.json();
 
     res.send(`
