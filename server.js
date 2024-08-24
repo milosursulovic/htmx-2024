69ca4691ba02517420cd686cf0168a5b63f052cd
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-24 16:14:42 +0200

    Get users from real api and add loading

--- server.js
+++ server.js
@@ -8,18 +8,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/users", (req, res) => {
-  const users = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Bob Williams" },
-    { id: 3, name: "Shannon Jackson" },
-  ];
+  setTimeout(async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
 
-  res.send(`
+    res.send(`
         <h1 class="text-2xl font-bold my-4">Users</h1>
         <ul>
             ${users.map((user) => `<li>${user.name}</li>`).join("")}
         </ul>
     `);
+  }, 2000);
 });
 
 app.listen(3000, () => {
